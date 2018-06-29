34c85dc9cde2f789dd99ae1676464a1131b92802
Wesley <wesleyburch@email.arizona.edu>
2018-06-28 22:27:41 -0700

    Finishing
    completed social section and contact section. added animations and form
    functionality. finished but can make small tweaks later.

--- source/js/app.js
+++ source/js/app.js
@@ -1,19 +1,49 @@
+//JQuery
+$( document ).ready(function() {
 
-// JavaScript
-window.sr = ScrollReveal(); 
+  $(".intro-text").animate({'opacity':'1'},1700);
+ 
+  $("#hide-project-1").click(function() {
+    $("#hide-1").toggle(300);
+    $("#hide-button-1").toggleClass("rotate-on-click");
+  });
 
-// sr.reveal('h1', {
-//     delay: 0,
-//     duration: 200,
-//     origin: 'bottom',
-//     distance: '100px' 
-// });
+  $("#hide-project-2").click(function() {
+    $("#hide-2").toggle(300);
+    $("#hide-button-2").toggleClass("rotate-on-click");
+  });
 
-function toggleHide(){
-  hiddenText = document.getElementsByClassName("hidden");
-  if(hiddenText.style.visibility === "hidden"){
-    hiddenText.style.visibility = "visible";
-  } else {
-    hiddenText.style.display = "hidden"
-  }
-}
+  $("#hide-project-3").click(function() {
+    $("#hide-3").toggle(300);
+    $("#hide-button-3").toggleClass("rotate-on-click");
+  });
+
+  $("#hide-project-4").click(function() {
+    $("#hide-4").toggle(300);
+    $("#hide-button-4").toggleClass("rotate-on-click");
+  });
+
+  $(window).scroll(function(){
+    // fade out hero triangle
+    $(".triangle-hero").css("opacity", 1 - $(window).scrollTop() / 600);
+  
+    $('.wrapper-starter').fadeIn('slow');
+    $('.section-header').each( function(i){
+              
+      var bottom_of_object = $(this).position().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      
+      /* If the object is completely visible in the window, fade it in */
+      if( bottom_of_window > bottom_of_object ){
+          
+          $(this).animate({'opacity':'1'},1700);
+              
+      }
+      
+    }); 
+
+  });
+
+});
+
+
